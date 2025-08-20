4b1d8aea7e15ccd5bdf71f1073cf1df8ce0b4b17
surya2prakash <sv251171@gmail.com>
2025-08-20 19:40:42 +0530

    followerModel

---
+++ backend/Model/followerModel.js
@@ -0,0 +1,28 @@
+
+const mongoose = require("mongoose");
+
+const followerSchema = new mongoose.Schema({
+       userId:{
+        type:mongoose.Schema.Types.ObjectId,
+         ref:"User"
+        },
+       followerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+       },
+
+        createdAt:{
+        type:Date,
+        default:Date.now
+       },
+       updatedAt:{
+        type:Date,
+        default:Date.now
+       }
+     
+});
+
+
+
+module.exports = mongoose.model("Follower",followerSchema);
