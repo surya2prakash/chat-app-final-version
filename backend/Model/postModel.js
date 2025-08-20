1b96cd3f20fc29f402c7ffd8ff0001155704f147
surya2prakash <sv251171@gmail.com>
2025-08-20 19:41:58 +0530

    post model

---
+++ backend/Model/postModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+
+const postSchema = new mongoose.Schema({
+      userId:{
+        //kiski post hai 
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+      },
+      imageUrl:{
+        type:String,
+        required:[true,"image url required"]
+      },
+      likeCount:{
+        type:Number,
+        default:0
+      },
+      commentCount:{
+        type:Number,
+        default:0
+      },
+      likeId:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Like",
+        
+      }],
+      commentId:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+      }],
+      createdAt:{
+         type:Date,
+          default:Date.now
+      },
+      updatedAt:{
+        type:Date,
+        default:Date.now
+      }
+});
+
+
+
+module.exports = mongoose.model("Post",postSchema);
