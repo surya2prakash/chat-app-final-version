0d7ecdc08eb467d22ad29798b5f2a6d40e3d2c40
surya2prakash <sv251171@gmail.com>
2025-08-28 23:09:07 +0530

    fetch posts from backend

---
+++ frontend/src/Component/Post/PostList.js
@@ -0,0 +1,52 @@
+   import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+import PostCard from './PostCard';
+   
+   export default function PostList() {
+
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+          async function getMyAllPost()
+          {
+
+            let token = localStorage.getItem("token");
+            try{
+              
+              
+                const response = await axios.get("http://localhost:5000/v1/myposts",{
+                    headers:{
+                        "Authorization":`Bearer ${token}`
+                    }
+                });
+
+                    let result = await response.data;
+                  toast.success(result?.message);
+                 setPosts(response?.data?.post);
+
+
+            }catch(err){
+              if(err?.response?.data)
+              {
+                 toast.error(err.response?.data?.message)
+              }else{
+                toast.error("NetWork Error !!");
+              }
+
+            }
+
+          }
+
+          getMyAllPost();
+    },[]);
+     return (
+       <div>
+               {
+                
+                  posts.map((post,index)=>(<PostCard key={post._id} post={post} />))
+               }
+       </div>
+     )
+   }
+   
