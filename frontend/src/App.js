640f78c2fc80840d69796c4cf02fa855779ce41d
surya2prakash <sv251171@gmail.com>
2025-08-30 23:38:01 +0530

    Chnages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Component/User/Login';
 import { useState } from 'react';
@@ -10,27 +10,28 @@ import MessageChat from './Component/Pages/MessageChat';
 import Explore from './Component/Pages/Explore';
 import Notification from './Component/Pages/Notification';
 import MyProfile from './Component/Pages/MyProfile';
+import Protected from './Component/ProtectedRoute/Protected';
 
 function App() {
    
-    const [isLogIn , setLogIn] = useState(false);
+    const [isLogIn , setLogIn] = useState(()=>{
+        return !!localStorage.getItem("token");
+    });
 
   return (
     <div className="App">
            <Routes>
                 <Route path='/' element={<Login setLogIn={setLogIn}/>}></Route>
                 <Route path='/signUp' element={<SignUp/>}></Route>
-                <Route path='/profile' element={
-                  
-                    isLogIn ? (<Profile/>):(<Navigate to='/'/>)
-                  } >
+                <Route path='/profile' element={<Protected isLogIn={isLogIn}><Profile/></Protected>}
+                >
                   <Route path='post' element ={
-                      isLogIn ?(<PostCreate/>):(<Navigate to="/" />) 
+                      <PostCreate/> 
                   }/>
-                  <Route path='chat' element = {isLogIn ?(<MessageChat/>) :(<Navigate to='/'/>)} />
-                  <Route path='explore' element = {isLogIn ?(<Explore/>) :(<Navigate to='/'/>)} />
-                  <Route path='notification' element = {isLogIn ?(<Notification/>) :(<Navigate to='/'/>)} />
-                  <Route path='myprofile' element = {isLogIn ?(<MyProfile/>) :(<Navigate to='/'/>)} />
+                  <Route path='chat' element = {<MessageChat/> } />
+                  <Route path='explore' element = {<Explore/> } />
+                  <Route path='notification' element = {<Notification/>} />
+                  <Route path='myprofile' element = {<MyProfile/>} />
                   
                   </Route>
            </Routes>
