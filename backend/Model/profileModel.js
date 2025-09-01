9d150187fe1b773b8d56b5422b5196e62482dc26
surya2prakash <sv251171@gmail.com>
2025-09-01 12:57:35 +0530

    userProfile

---
+++ backend/Model/profileModel.js
@@ -0,0 +1,39 @@
+
+const mongoose = require("mongoose");
+
+
+const profileSchema = new mongoose.Schema({
+  
+      userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+     },
+       
+     
+    totalFollowers:{
+          type:Number,
+          
+          default:0
+      },
+      totalfollowing:{
+         type:Number,
+         
+         default:0
+      },
+      totalPost:{
+         type:Number,
+         
+         default:0
+      },
+      createdAt:{
+         type:Date,
+         default:Date.now
+      },
+      updatedAt:{
+        type:Date,
+        default:Date.now
+      }
+
+});
+
+module.exports = mongoose.model("Profile",profileSchema);
