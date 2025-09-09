e46381e028fbe937f5e26cb0815b94c917972828
surya2prakash <sv251171@gmail.com>
2025-09-09 18:30:09 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ import Explore from './Component/Pages/Explore';
 import Notification from './Component/Pages/Notification';
 import MyProfile from './Component/Pages/MyProfile';
 import Protected from './Component/ProtectedRoute/Protected';
+import OthersProfile from './Component/OtherComponents/OthersProfile';
+import EditProfile from './Component/OtherComponents/EditProfile';
+import Chat from './Component/Pages/Chat';
 
 function App() {
    
@@ -18,21 +21,27 @@ function App() {
         return !!localStorage.getItem("token");
     });
 
+    const [getImageProfile,setImageProfile]=useState("");
+
   return (
     <div className="App">
            <Routes>
                 <Route path='/' element={<Login setLogIn={setLogIn}/>}></Route>
                 <Route path='/signUp' element={<SignUp/>}></Route>
-                <Route path='/profile' element={<Protected isLogIn={isLogIn}><Profile/></Protected>}
+                <Route path='/profile' element={<Protected isLogIn={isLogIn}><Profile profileImage={getImageProfile}/></Protected>}
                 >
                   <Route path='post' element ={
                       <PostCreate/> 
                   }/>
-                  <Route path='chat' element = {<MessageChat/> } />
+                  <Route path='chats' element = {<MessageChat/> } />
                   <Route path='explore' element = {<Explore/> } />
                   <Route path='notification' element = {<Notification/>} />
-                  <Route path='myprofile' element = {<MyProfile/>} />
-                  
+                  <Route path='myprofile' element = {<MyProfile imgProfile={setImageProfile}/>} />
+                      
+                  <Route path='userProfile/:id' element={<OthersProfile/>} />
+                  <Route path='editProfile' element={<EditProfile/>}/>
+                  <Route path='chat' element={<Chat/>}/>
+                 
                   </Route>
            </Routes>
     </div>
